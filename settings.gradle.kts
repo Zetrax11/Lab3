aa00a9ae74d6492242b4b9607f268f13b8ddd27b
Zetrax11 <150843006+Zetrax11@users.noreply.github.com>
2023-11-28 16:56:43 +0100

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "L3_268524"
+include(":app")
+ 
